93781ea5ef7e969d859fa6138202be28cd8f3472
Lotta <lmikkonen@hubspot.com>
2016-11-29 22:22:09 +0000

    fixed type

--- transfer/git.sh
+++ transfer/git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-git pull && find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {}\;
+git pull && find . -type d -exec chmod 755 {} \; && find . -type f -exec chmod 644 {} \;
 chmod +x git.sh
